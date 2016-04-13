80d94723759d891a0ff36df6ace6ca1fd50c6f2d
rileyharrison <rileyharrison@gmail.com>
2016-04-13 10:18:07 -0500

    database connection updated

--- modules/connection.js
+++ modules/connection.js
@@ -1,12 +1,20 @@
 // connection.js
 var connectionString = '';
 
-if(process.env.DATABASE_URL != undefined) {
-    connectionString = process.env.DATABASE_URL + 'ssl';
-} else {
-    // connectionString = 'postgres://localhost:5432/passport-users';
-    connectionString = 'postgres://localhost:5432/db_meal_planner';
 
+if (process.env.DATABASE_URL){
+    pg.defaults.ssl=true;
+    connectionString = process.env.DATABASE_URL;
+} else {
+    connectionString = 'postgress://localhost:5432/db_meal_planner';
 }
+
+// if(process.env.DATABASE_URL != undefined) {
+//     connectionString = process.env.DATABASE_URL + 'ssl';
+// } else {
+//     // connectionString = 'postgres://localhost:5432/passport-users';
+//     connectionString = 'postgres://localhost:5432/db_meal_planner';
+//
+// }
 
 module.exports = connectionString;
