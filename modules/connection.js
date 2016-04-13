f7992ceb22a6084490484feacf1be28429297b5d
rileyharrison <rileyharrison@gmail.com>
2016-04-13 10:51:11 -0500

    more database connect added require pg

--- modules/connection.js
+++ modules/connection.js
@@ -1,20 +1,20 @@
 // connection.js
 var connectionString = '';
 
-
-// if (process.env.DATABASE_URL){
-//     pg.defaults.ssl=true;
-//     connectionString = process.env.DATABASE_URL;
-// } else {
-//     connectionString = 'postgress://localhost:5432/db_meal_planner';
-// }
-
-if(process.env.DATABASE_URL != undefined) {
-    connectionString = process.env.DATABASE_URL + 'ssl';
+var pg = require('pg');
+if (process.env.DATABASE_URL){
+    pg.defaults.ssl=true;
+    connectionString = process.env.DATABASE_URL;
 } else {
-    // connectionString = 'postgres://localhost:5432/passport-users';
-    connectionString = 'postgres://localhost:5432/db_meal_planner';
-
+    connectionString = 'postgress://localhost:5432/db_meal_planner';
 }
+
+// if(process.env.DATABASE_URL != undefined) {
+//     connectionString = process.env.DATABASE_URL + 'ssl';
+// } else {
+//     // connectionString = 'postgres://localhost:5432/passport-users';
+//     connectionString = 'postgres://localhost:5432/db_meal_planner';
+//
+// }
 
 module.exports = connectionString;
