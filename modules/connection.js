e1e48e378874b73c3c8422921835bd1060070af3
rileyharrison <rileyharrison@gmail.com>
2016-04-13 10:24:26 -0500

    trouleshoot database

--- modules/connection.js
+++ modules/connection.js
@@ -2,19 +2,19 @@
 var connectionString = '';
 
 
-if (process.env.DATABASE_URL){
-    pg.defaults.ssl=true;
-    connectionString = process.env.DATABASE_URL;
-} else {
-    connectionString = 'postgress://localhost:5432/db_meal_planner';
-}
-
-// if(process.env.DATABASE_URL != undefined) {
-//     connectionString = process.env.DATABASE_URL + 'ssl';
+// if (process.env.DATABASE_URL){
+//     pg.defaults.ssl=true;
+//     connectionString = process.env.DATABASE_URL;
 // } else {
-//     // connectionString = 'postgres://localhost:5432/passport-users';
-//     connectionString = 'postgres://localhost:5432/db_meal_planner';
-//
+//     connectionString = 'postgress://localhost:5432/db_meal_planner';
 // }
+
+if(process.env.DATABASE_URL != undefined) {
+    connectionString = process.env.DATABASE_URL + 'ssl';
+} else {
+    // connectionString = 'postgres://localhost:5432/passport-users';
+    connectionString = 'postgres://localhost:5432/db_meal_planner';
+
+}
 
 module.exports = connectionString;
